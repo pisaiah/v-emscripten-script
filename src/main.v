239e610a89bf70bd5cde968d2411f18eca2683af
Isaiah <16439221+pisaiah@users.noreply.github.com>
2024-10-15 02:14:27 -0400

    Update wasm.js

--- src/main.v
+++ src/main.v
@@ -490,26 +490,34 @@ fn output_helper_js() {
 import loadWASM from "./app.js";
 
 function doo_load_files() {
-	for (var i = 0; i < localStorage.length; i++) { var key = localStorage.key(i);
-		if (key.endsWith(".ttf")) { continue; }
-		write_file(key.replace("//", "/"), localStorage.getItem(key));
-	}
+	setTimeout(function() { 
+		for (var i = 0; i < localStorage.length; i++) { var key = localStorage.key(i);
+			if (key.endsWith(".ttf")) { continue; }
+			write_file(key.replace("//", "/"), localStorage.getItem(key));
+		}
+	}, 1);
 }
 
 function write_file(a, b) {
-	var dirr = a.substring(0, a.lastIndexOf("/")); var dd = "";
-	var spl = dirr.split("/");
-	for (var i = 0; i < spl.length; i++) { var da = spl[i]; dd += da + "/"; try { iui.module.FS.mkdir(dd) } catch (exx) {} }
-	try { iui.module.FS.mkdir(dirr) } catch (exx) {}
-	iui.module.FS.writeFile(a, b)  
+	setTimeout(function() { 
+		var dirr = a.substring(0, a.lastIndexOf("/")); var dd = "";
+		var spl = dirr.split("/");
+		for (var i = 0; i < spl.length; i++) { var da = spl[i]; dd += da + "/"; try { iui.module.FS.mkdir(dd) } catch (exx) {} }
+		try { iui.module.FS.mkdir(dirr) } catch (exx) {}
+		iui.module.FS.writeFile(a, b);
+	}, 1);
 }
 
 function save_folder(pa) {
+	setTimeout(function() { save_folder_2(pa); }, 1); // wasm crashes if we load FS too early
+}
+
+function save_folder_2(pa) {
 	var is_file = iui.module.FS.isFile(iui.module.FS.stat(pa).mode)
 	if (!is_file) {
 		var last = pa.substring(pa.lastIndexOf("/") + 1, pa.length);
 		if (last.length <= 2 && last.includes(".")) { return; }
-		var lss = iui.module.FS.readdir(pa); for (var i = 0; i < lss.length; i++) { save_folder(pa + "/" + lss[i]); }
+		var lss = iui.module.FS.readdir(pa); for (var i = 0; i < lss.length; i++) { save_folder_2(pa + "/" + lss[i]); }
 	} else { var con = iui.module.FS.readFile(pa, { encoding: "utf8" }); localStorage.setItem(pa, con); }
 }
 
@@ -554,7 +562,7 @@ window.iui = {
 		if (val == "openfiledialog"){ return iui.open_file_dialog(); } else if (val == "savefiledialog"){ iui.save_file_dialog(); }
 		else if (val == "keyboard-hide"){ document.getElementById("canvas").focus(); navigator.virtualKeyboard.hide(); }
 		else if (val == "keyboard-show"){ document.getElementById("canvas").focus(); navigator.virtualKeyboard.show(); }
-		else if (val == "lloadfiles") { doo_load_files(); } else if (val == "savefiles") { save_folder("/home/") }
+		else if (val == "lloadfiles") { doo_load_files(); } else if (val == "savefiles") { save_folder("/home") }
 		else if (val.indexOf("savefile=") != -1) { var fi_nam = val.split("savefile=")[1]; iui.download_file(fi_nam, iui.module.FS.readFile(fi_nam)); }
 	}
 };
